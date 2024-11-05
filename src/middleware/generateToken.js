e397709dc73d8e4ab1db64ce8b4ced57575ae18b
idlakshan <id.lakshan21@gmail.com>
2024-11-05 10:41:29 +0530

    feat: add generate token

---
+++ src/middleware/generateToken.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const User = require('../users/user.model');
+
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
+
+const generateToken = async (userId) => {
+    try {
+        const user = await User.findById(userId);
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        const token = jwt.sign(
+            { userId: user._id, role: user.role },
+            JWT_SECRET_KEY,
+            { expiresIn: "1h" }
+        );
+        
+        return token;
+    } catch (error) {
+        console.error("Error generating token:", error.message);
+        throw new Error("Could not generate token");
+    }
+};
+
+module.exports = generateToken;
