8636cf32ce6f408ee9a793d0e69e961216445418
idlakshan <id.lakshan21@gmail.com>
2024-11-18 13:59:17 +0530

    feat add Reviews Schema and Model

--- src/products/products.model.js
+++ src/products/products.model.js
@@ -1,4 +1,4 @@
-const { Schema, default: mongoose } = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 
 const ProductSchema=new Schema({
@@ -28,13 +28,13 @@ const ProductSchema=new Schema({
     rating:{
         type:Number,
         default:0
+       
     },
     author:{
-        type:mongoose.Types.ObjectId,
+        type:Schema.Types.ObjectId,
         ref:"User",
         required:true
     }
 })
-
-const Products=mongoose.model("Products",ProductSchema)
+const Products = model("Products",ProductSchema);
 module.exports=Products;
